54c766f621acf5f21e4be2306ea3f8992187bdfe
Mark Turner <mark@amerine.net>
2009-09-05 23:57:34 -0700

    Added all the models, controllers, views, etc.. for authlogic and user account, use sessions, etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,19 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  filter_parameter_logging :password, :password_confirmation
+  helper_method :current_user_session, :current_user
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'ddf58be1d47581d5acc66429217a3638'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :user_session
+  map.resource :account, :controller => 'users'
+  map.resources :users
+  
   map.root :controller => "welcome"
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
